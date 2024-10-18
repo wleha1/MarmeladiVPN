db542b7efd5872615e6ab42e7a77dc26624d1e9a
wleha1 <klerens1221@gmail.com>
2024-10-18 13:06:17 +0300

    progress

--- marmeladi-vpn/src/App.jsx
+++ marmeladi-vpn/src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.js
 import Main from './pages/main/Main';
+import Register from './pages/register/Reg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
     <Routes>
-          <Route path="/" element={<Main />} />
+         <Route path="/" element={<Main />} />
+          <Route path="/register" element={<Register />} />
     </Routes>
 </Router>
   )
